e3996e69276932b0dbbb95690ceabadac1d2eb19
Pedro <fulanodetal@exempldo.br>
2026-05-07 15:50:20 -0300

    feat:add company details api

--- service/src/app/validations/company.ts
+++ service/src/app/validations/company.ts
@@ -1,4 +1,4 @@
-import { z } from "zod";
+import {  z } from "zod";
 
 export const createCompanySchema = z.object({
   name: z.string().min(2, "nome da empresa é obrigatório"),
@@ -9,4 +9,24 @@ export const createCompanySchema = z.object({
   address: z.string().optional(),
 });
 
+export const updateCompanySchema = createCompanySchema
+  .partial()
+  .refine((data) => Object.keys(data).length > 0, {
+    message: "envie pelo menos um campo para tualizar",
+  });
+
 export type CreateCompanyInput = z.infer<typeof createCompanySchema>;
+export type UpdateCompanyInput = z.infer<typeof updateCompanySchema>;
+
+
+//  enum Comida {
+//     APROVADO="aporvado",
+//     REPROVADO="reprovado",
+//     ANDANDO="ANDANDO",
+// }
+
+//  function Eu(params:Comida) {
+//     console.log(`status atual${params}`)
+// }
+// Eu(Comida.ANDANDO)
+
